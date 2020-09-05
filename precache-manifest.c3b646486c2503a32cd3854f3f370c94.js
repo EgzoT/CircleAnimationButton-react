a7ada569cc2b14f7bddcec9348e7baef5561463f
EgzoT <przemek7575@prokonto.pl>
2020-09-05 17:09:09 +0200

    Add gh-page app

---
+++ precache-manifest.c3b646486c2503a32cd3854f3f370c94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a37ee9e2a6325f75d4c4b2cc0b22144",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40916c2815e669a4c95d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "68b9a3be389b2d56e807",
+    "url": "/static/js/2.d0c1f3ea.chunk.js"
+  },
+  {
+    "revision": "05ea761b7a5361472988577479d7ab28",
+    "url": "/static/js/2.d0c1f3ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40916c2815e669a4c95d",
+    "url": "/static/js/main.2d73ebb6.chunk.js"
+  },
+  {
+    "revision": "67d35dad8d7ab5cd98ba",
+    "url": "/static/js/runtime-main.789658a6.js"
+  }
+]);
