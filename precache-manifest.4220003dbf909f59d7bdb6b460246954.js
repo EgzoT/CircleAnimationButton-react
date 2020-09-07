94b1874441d6481f0a7ec53ab6f6a8083d848877
EgzoT <przemek7575@prokonto.pl>
2020-09-07 20:41:36 +0200

    Update gh-pages to newest version

--- precache-manifest.4220003dbf909f59d7bdb6b460246954.js
+++ precache-manifest.4220003dbf909f59d7bdb6b460246954.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0a37ee9e2a6325f75d4c4b2cc0b22144",
+    "revision": "71b154d2e6c258c872189233ac26d6f3",
     "url": "/index.html"
   },
   {
-    "revision": "40916c2815e669a4c95d",
+    "revision": "a9fafb35409ae983a583",
     "url": "/static/css/main.5ecd60fb.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.d0c1f3ea.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "40916c2815e669a4c95d",
-    "url": "/static/js/main.2d73ebb6.chunk.js"
+    "revision": "a9fafb35409ae983a583",
+    "url": "/static/js/main.3626ade9.chunk.js"
   },
   {
     "revision": "67d35dad8d7ab5cd98ba",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c3b646486c2503a32cd3854f3f370c94.js"
+  "/precache-manifest.4220003dbf909f59d7bdb6b460246954.js"
 );
 
 self.addEventListener('message', (event) => {
